b46140f6e70f1be0853f247035bc9eea7e3b2593
minsoo <viviennes7@github.com>
2019-06-10 21:14:15 +0900

    remove logger

--- src/main/java/com/ms/ioexample/IoExampleApplication.java
+++ src/main/java/com/ms/ioexample/IoExampleApplication.java
@@ -1,7 +1,5 @@
 package com.ms.ioexample;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RestController
 public class IoExampleApplication {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
     private final AtomicInteger atomicInteger = new AtomicInteger();
 
     public static void main(String[] args) {
